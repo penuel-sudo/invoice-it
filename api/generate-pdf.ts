0f6de3cbb1e259d8c17f8c336a70caed2a6e7eda
Your Name <you@example.com>
2025-09-26 16:28:07 +0100

    whop

--- api/generate-pdf.ts
+++ api/generate-pdf.ts
@@ -1,5 +1,5 @@
 import { pdf } from '@react-pdf/renderer'
-import InvoicePDFTemplate from './InvoicePDFTemplate.js'
+import InvoicePDFTemplate from './InvoicePDFTemplate'
 import React from 'react'
 
 export default async function handler(req: any, res: any) {
